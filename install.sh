ab67a3ae2edd5f10476dbb1d713890322df7065c
Guillaume Ferrand <57199736+Red-Hide@users.noreply.github.com>
2022-03-09 17:02:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 #Install dependencies
 
 sudo apt update && sudo apt upgrade
-sudo apt install python3 git-core
+sudo apt install python3 git-core qt5-default pigpio
+sudo systemctl enable pigpiod
+sudo systemctl start pigpiod
 git clone https://github.com/Red-Hide/ZeroP_Software.git
 cd ZeroP_Software
 pip3 install -r requirements.txt
