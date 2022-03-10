0f84cd72554bf9924f61addd2b28d2e5c3ebb953
Guillaume Ferrand <57199736+Red-Hide@users.noreply.github.com>
2022-03-10 13:51:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #Install dependencies
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt install python3 git-core python3-pyqt5 pigpio -y
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
