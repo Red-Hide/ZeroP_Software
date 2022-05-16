182d16f3b30b2b5f2bb6eb335708086a19d4dfc2
Guillaume Ferrand <57199736+Red-Hide@users.noreply.github.com>
2022-05-16 15:08:44 +0200

    Added LCD driver and dev branch

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install python3 git-core python3-pyqt5 pigpio -y
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
-git clone https://github.com/Red-Hide/ZeroP_Software.git
+git clone -b dev https://github.com/Red-Hide/ZeroP_Software.git
 cd ZeroP_Software
+sudo bash ./LCD5-show
 pip3 install -r requirements.txt
